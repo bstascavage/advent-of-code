228bae9d00538c1199b607b513fb93841f44cf5c
stascavageb <brian.stascavage@saic.com>
2020-12-01 23:30:06 -0500

    [Day1]: Adding 'load_file' function

--- main.py
+++ main.py
@@ -2,19 +2,26 @@ EXPENSE_REPORT = "expense_report.txt"
 
 
 def main():
-    report = open(EXPENSE_REPORT, "r")
-    expense_list = report.read().split("\n")
-    report.close()
+    expense_list = load_file(EXPENSE_REPORT)
+
+    # Ensuring that every element in the list is set as an int
     expense_list = [int(i) for i in expense_list]
 
     print(expense_compare(expense_list))
 
 
-def expense_compare(expense_list):
-    expense_list.sort()
+def load_file(file):
+    input_file = open(file, "r")
+    output_list = input_file.read().split("\n")
+    input_file.close()
+    return output_list
 
-    for first_entry in expense_list:
-        for second_entry in expense_list:
+
+def expense_compare(list):
+    list.sort()
+
+    for first_entry in list:
+        for second_entry in list:
             if (first_entry + second_entry) == 2020:
                 return first_entry * second_entry
 
